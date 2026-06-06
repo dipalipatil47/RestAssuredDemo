dda3e5cf086a12630ef9545fea6ec737d267a250
dipalipatil47 <dipalipatil8390@gmail.com>
2026-06-07 00:27:37 +0530

    "changed statment"

--- src/test/java/Chaining/CreateUser.java
+++ src/test/java/Chaining/CreateUser.java
@@ -12,27 +12,25 @@ public class CreateUser {
 
 	@Test
 	public void test_CreateUser(ITestContext context) {
-		
-	 Faker faker= new Faker();
-	 JSONObject data= new JSONObject();
-	 data.put("name", faker.name().fullName());
-	 data.put("gender", "Female");
-	 data.put("email", faker.internet().emailAddress());
-	 data.put("status", "inactive");
-	 
-	 String bearerToken="b641943e9cdefcc6e203659c6a32e552abe0f19a8835759e4518c7f3a2867b92";
-	 int id= given()
-	 .headers("Authorization","Bearer "+bearerToken)
-	 .contentType("application/json; charset=utf-8")
-	 .body(data.toString())
-	 
-	 .when()
-	 .post("https://gorest.co.in/public/v2/users")
-	 .jsonPath().getInt("id");
-	 System.out.println("Generated id:"+id);
-	 System.out.println("User created");
-	 
-	 context.setAttribute("user_id", id);
+
+		Faker faker = new Faker();
+		JSONObject data = new JSONObject();
+		data.put("name", faker.name().fullName());
+		data.put("gender", "Female");
+		data.put("email", faker.internet().emailAddress());
+		data.put("status", "inactive");
+		System.out.println("User created......");
+
+		String bearerToken = "b641943e9cdefcc6e203659c6a32e552abe0f19a8835759e4518c7f3a2867b92";
+		int id = given().headers("Authorization", "Bearer " + bearerToken)
+				.contentType("application/json; charset=utf-8").body(data.toString())
+
+				.when().post("https://gorest.co.in/public/v2/users").jsonPath().getInt("id");
+		System.out.println("Generated id:" + id);
+		System.out.println("User created");
+
+		context.setAttribute("user_id", id);
+
 	}
-	
+
 }
